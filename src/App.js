5cb1a383733c436e2f75f7df90b3d14f3b3e0706
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-09-24 10:14:55 +0100

    styling modified

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-<<<<<<< HEAD
-import React from 'react';
-=======
 import './index.css';
 import React, { useEffect } from 'react';
->>>>>>> 8f7397426e6a5f33b7e3e9a578e552a09dde132d
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Categories from './components/Categories';
+import { fetchBooks } from './redux/books/books';
 import Books from './components/Books';
 
 const App = () => {
   const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  useEffect(() => async () => {
+    await dispatch(fetchBooks());
+  }, []);
 
   return (
     <div className="App">
